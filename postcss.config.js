89cd90835bb10d215cf132aab44ee12ce3249a8b
Brian Sipple <brsipple42@gmail.com>
2017-07-27 07:10:03 -0700

    initial project build

---
+++ postcss.config.js
@@ -0,0 +1,40 @@
+/* eslint-env node */
+
+const path = require('path');
+
+module.exports = ctx => {
+  const { file: { basename: CURRENT_FILE_BASE_NAME, dirname: fileDirName } } = ctx;
+
+  return {
+    plugins: {
+      // ⚠️ Order matters! PostCSS will run plugins in the order listed.
+      stylelint: {
+        configFile: path.join(__dirname, 'stylelint.config.js')
+      },
+
+      'postcss-import': {
+        root: fileDirName
+      },
+
+      'postcss-cssnext': {
+        features: {
+          customProperties: {
+            preserve: CURRENT_FILE_BASE_NAME !== 'index.css'
+          }
+        }
+      },
+
+      // 🔑 Set `process.env.NODE_ENV` to "production" to create
+      // a minified production build
+      cssnano: ctx.env === 'production' ? {} : false,
+
+      'postcss-reporter': {
+        clearAllMessages: true,
+        throwError: true,
+        plugins: [
+          'stylelint'
+        ]
+      }
+    }
+  };
+};
